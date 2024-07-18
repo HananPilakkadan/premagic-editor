9673b321f9d703f42b25f43529d5bd4f2a506b68
hananpilakkadan <hanan.p@webandcrafts.com>
2024-07-19 00:58:29 +0530

    toast added

--- src/components/EditorControls/useEditorControls.js
+++ src/components/EditorControls/useEditorControls.js
@@ -101,7 +101,7 @@ export const useEditorControls = () => {
     document.body.removeChild(link);
 
     URL.revokeObjectURL(link.href);
-    // return JSON.stringify(filterData, null, 2);
+    toast.success("JSON metadata is downloaded!");
   };
   const handleColorClick = (colorName) => {
     dispatch(handleColorChange(colorName));
@@ -111,10 +111,11 @@ export const useEditorControls = () => {
     const dataUrl = canvas.toDataURL("image/png");
     const a = document.createElement("a");
     a.href = dataUrl;
-    a.download = "edited-image.png";
+    a.download = "premagic-edited-image.png";
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
+    toast.success("Image downloaded!");
   };
 
   const handleInputText = (event) => {
